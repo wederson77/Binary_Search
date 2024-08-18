4091aa2f7478a48572348a942a4300065806483e
wenderson77 <wendersonpina10@gmail.com>
2024-08-17 23:31:51 -0300

    binary_search_simple

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod binary_search;
+use binary_search::binary_search;
+
+fn main() {
+    let nums = vec![1,2,3,4,5,6,7,8,9, 10];
+    let target = 7;
+    let result = binary_search(&nums, target);
+    if result == -1{
+        println!("O numero {} não foi encontrado no vetor.", target);
+    }else{
+        println!("O numero {} foi encontrado no indice {} do vetor.", target, result);
+    }
+}
